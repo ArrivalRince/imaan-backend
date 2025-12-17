3c845892d77ba8fe8d57d97a9dedc5ec88a4ac58
Naisya Harjimen <nsyaharjimen@gmail.com>
2025-12-17 12:09:04 +0700

    CRUD Kegiatan

--- src/routes/KegiatanRoutes.js
+++ src/routes/KegiatanRoutes.js
@@ -7,6 +7,8 @@ import {
   deleteKegiatan
 } from "../controllers/KegiatanController.js";
 
+import upload from "../middleware/upload.js";
+
 const router = express.Router();
 
 // GET semua kegiatan
@@ -16,10 +18,10 @@ router.get("/", getKegiatan);
 router.get("/:id", getKegiatanById);
 
 // POST create kegiatan
-router.post("/", createKegiatan);
+router.post("/", upload.single("foto_kegiatan"), createKegiatan);
 
 // UPDATE
-router.put("/:id", updateKegiatan);
+router.put("/:id", upload.single("foto_kegiatan"), updateKegiatan);
 
 // DELETE
 router.delete("/:id", deleteKegiatan);
